27405fdfaef0d9664b5734255babf512527b355c
Thandiwe <tembot@github.com>
2022-04-11 14:41:39 -0400

    Scatter plot showing; needs work

--- finalClassProject/chart1.js
+++ finalClassProject/chart1.js
@@ -1,18 +1,9 @@
-/* var prisonData = d3.csv("ThePrisonIndustry2020.csv");
-
-prisonData.then(function(csvFile){
-    console.log(csvFile);
-})
-.catch(function(error){
-    console.log(error);
-});*/
-
-// set the dimensions and margins of the graph
+// set dimensions and margins of graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 1500 - margin.left - margin.right,
     height = 1600 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+// append svg object to the body of the page
 var svg = d3.select("#chart1")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -21,18 +12,18 @@ var svg = d3.select("#chart1")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
+//Read data
 d3.csv("ThePrisonIndustryData2020.csv", function(data) {
 
-  // Add X axis
+  // X axis
   var x = d3.scaleLinear()
-    .domain([0, 1550])
+    .domain([0, 1500])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
-    // Add Y axis
+    // Y axis
   var y = d3.scaleLinear()
   .domain([0, 400000])
   .range([ height, 0]);
@@ -40,16 +31,27 @@ d3.csv("ThePrisonIndustryData2020.csv", function(data) {
   svg.append("g")
   .call(d3.axisLeft(y));
 
-  console.log("add dot: " + data.Rank + ", " + data.AnnualRevenuesMn);
+  //console.log("add dot: " + data.Rank + ", " + data.AnnualRevenuesMn);
 
-
-  svg.selectAll("dot")
-      .data(data)
-    .enter().append("circle")
+ // if (data.Rank <= 100){
+  svg.append("circle")
       .attr("r", 5)
-      .attr("cx", function(d) { return x(d.Rank); })
-      .attr("cy", function(d) { return y(d.AnnualRevenuesMn); })
-      .style("fill", "#69b3a2");;
+      .attr("cx", data.Rank)
+      .attr("cy", data.AnnualRevenuesMn)
+      .style("fill", "#4287f5");
+ // };
 
   console.log("done");
 })
+
+d3.select("#chart1")
+  .on("mousemove", function(event) {
+    d3.select("#tooltip")
+      .style("display", "block")
+      .style("top", event.pageY + 20 + "px")
+      .style("left", event.pageX + 20 + "px")})
+
+      .on("mouseout", function() {
+        d3.select("#tooltip")
+          .style("display", "none");
+  });
