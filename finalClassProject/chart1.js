8aa51287b552f8fb7ff20da3e527a6115912751e
Thandiwe <tembot@github.com>
2022-04-14 16:39:31 -0400

    Scatter plot with colored nodes for each country

--- finalClassProject/chart1.js
+++ finalClassProject/chart1.js
@@ -1,7 +1,9 @@
 // set dimensions and margins of graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 1500 - margin.left - margin.right,
-    height = 1600 - margin.top - margin.bottom;
+    width = (screen.width-600) - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+
+    console.log(screen.width)
 
 // append svg object to the body of the page
 var svg = d3.select("#chart1")
@@ -13,11 +15,11 @@ var svg = d3.select("#chart1")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read data
-d3.csv("ThePrisonIndustryData2020.csv", function(data) {
+d3.csv("ThePrisonIndustryData2020Cleaned.csv", function(data) {
 
   // X axis
   var x = d3.scaleLinear()
-    .domain([0, 1500])
+    .domain([0, 700000])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -25,33 +27,83 @@ d3.csv("ThePrisonIndustryData2020.csv", function(data) {
 
     // Y axis
   var y = d3.scaleLinear()
-  .domain([0, 400000])
+  .domain([0, 300000])
   .range([ height, 0]);
   
   svg.append("g")
   .call(d3.axisLeft(y));
 
-  //console.log("add dot: " + data.Rank + ", " + data.AnnualRevenuesMn);
-
- // if (data.Rank <= 100){
   svg.append("circle")
-      .attr("r", 5)
-      .attr("cx", data.Rank)
-      .attr("cy", data.AnnualRevenuesMn)
-      .style("fill", "#4287f5");
- // };
+      .attr("r", 3)
+      .attr("cx", data.Employees.replace(/\s/g, ''))
+      .attr("cy", data.AnnualRevenuesMn.replace(/\s/g, ''))
+      .style("fill", function() {
+        switch(data.Continent) {
+        case 'North America':
+          return "#F9ACFA";
+        case 'Africa':
+          return "#E000D1";
+        case 'Asia':
+          return "#FAF8A9";          
+        case 'Europe':
+          return '#5F80FA';          
+        case 'South America':
+          return "#FF5026";
+        case 'Australia':
+          return "#82FFD2";
+        }
+      })
 
-  console.log("done");
+    /*svg.selectAll("circle")
+    .transition()
+    .delay(function(d,i){return(i*3)})
+    .duration(2000)
+    .attr("cx", x(data.Employees.replace(/\s/g, '')) )
+    .attr("cy", y(data.AnnualRevenuesMn.replace(/\s/g, '')) )*/
+    
+      console.log(data.Continent);
 })
+console.log("scatter plot done");
 
-d3.select("#chart1")
-  .on("mousemove", function(event) {
-    d3.select("#tooltip")
-      .style("display", "block")
-      .style("top", event.pageY + 20 + "px")
-      .style("left", event.pageX + 20 + "px")})
 
-      .on("mouseout", function() {
-        d3.select("#tooltip")
-          .style("display", "none");
-  });
+
+  /* append svg object to the body of the page
+var svg = d3.select("#chart2")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+//Read data
+d3.csv("ThePrisonIndustryData2020.csv", function(data) {
+
+// X axis
+var bubblePlotX = d3.scaleLinear()
+  .domain([0, 30])
+  .range([ 0, width ]);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+
+  // Y axis
+var BubblePlotY = d3.scaleLinear()
+.domain([0, 400000])
+.range([ height, 0]);
+
+svg.append("g")
+.call(d3.axisLeft(BubblePlotY));
+
+//console.log("add dot: " + data.Rank + ", " + data.AnnualRevenuesMn);
+
+if (data.Rank <= 25){
+svg.append("circle")
+    .attr("r", data.AnnualRevenuesMn/3)
+    .attr("cx", data.rank)
+    .attr("cy", d3.randomUniform(10, 25))
+    .style("outline", "#4287f5");
+};
+
+console.log("bubble chart done");
+})*/
